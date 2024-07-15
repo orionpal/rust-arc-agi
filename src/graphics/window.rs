592afb0639488e51aed6089280808a21d9f03909
Orion Palaquibay <23282980+orionpal@users.noreply.github.com>
2024-07-14 21:27:33 -0400

    just playing around with visuals

--- src/graphics/window.rs
+++ src/graphics/window.rs
@@ -1,20 +1,32 @@
 use bevy::prelude::*;
+use bevy::sprite::{MaterialMesh2dBundle, Mesh2dHandle};
 
 pub fn start_app() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup_camera)
+        .add_systems(Startup, setup)
         .run();
 }
 #[derive(Component)]
 struct MyCameraMarker;
 
-fn setup_camera(mut commands: Commands) {
+fn setup(mut commands: Commands,
+         mut meshes: ResMut<Assets<Mesh>>,
+         mut materials: ResMut<Assets<ColorMaterial>>) {
     commands.spawn((
-        Camera2dBundle {
-            transform: Transform::from_xyz(100.0, 200.0, 0.0),
-            ..default()
-        },
+        Camera2dBundle::default(),
         MyCameraMarker,
     ));
+    let shape = Mesh2dHandle(meshes.add(Rectangle::new(50.0, 50.0)));
+    let color = Color::rgb(0.0,55.0,0.0);
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: shape,
+        material: materials.add(color),
+        transform: Transform::from_xyz(
+            50.0,
+            0.0,
+            0.0,
+        ),
+        ..default()
+    });
 }
